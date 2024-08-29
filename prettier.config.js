ca5f95e1dedf219ebc218f90697940f92517fe66
Alexandr Petyaykin <saneksa2@gmail.com>
2024-08-29 16:00:50 +0300

    chore: обновлены библиотеки

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/**
+ * @type {import("prettier").Options}
+ */
+module.exports = require("@infomaximum/config/prettierconfig.common.js");
